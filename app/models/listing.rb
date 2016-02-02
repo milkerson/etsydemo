1b585f19a50d006b2ebd3b6af8685c8f768ad8f5
Mike Wilkerson <wilkerson@mac.com>
2016-02-02 07:22:56 -0600

    Added banner to homepage

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,9 +3,9 @@ class Listing < ActiveRecord::Base
 	belongs_to :user
 
 	if Rails.env.development?
-		has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default.jpg"
+		has_attached_file :image, styles: { medium: "200x>", thumb: "100x60#" }, default_url: "default.jpg"
 	else
-		has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default.jpg",
+		has_attached_file :image, styles: { medium: "200x>", thumb: "100x60#" }, default_url: "default.jpg",
 			:storage => :dropbox,
 	    	:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 	    	:path => ":style/:id_:filename"
