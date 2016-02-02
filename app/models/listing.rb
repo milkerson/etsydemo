36ba934bb1767da8a928e617e73735ade315b901
Mike Wilkerson <wilkerson@mac.com>
2016-02-02 16:42:38 -0600

    enable users to place orders

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,7 @@
 class Listing < ActiveRecord::Base
 
 	belongs_to :user
+	has_many :orderd
 
 	if Rails.env.development?
 		has_attached_file :image, styles: { medium: "200x>", thumb: "100x60#" }, default_url: "default.jpg"
